d85144ba218715b0228a2616c748db3018ba0b52
martin rabaglia <martinrabaglia@martins-MacBook-Pro.local>
2015-08-21 15:25:43 +0200

    front end

--- server/public/javascripts/main.js
+++ server/public/javascripts/main.js
@@ -2,6 +2,6 @@ var socket = io();
 
 socket.on('grow',function(message){
 	console.log(message);
-	var size = Math.random() * 50 + "px";
-	d3.select("h1.change").transition().style("font-size", size);
+	var size = Math.random() * 100 + "px";
+	d3.select("h1.mutate").transition().style("font-size", size);
 })
